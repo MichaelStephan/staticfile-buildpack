a52e1b59b4e8f17109d66ea0df28c79aad270947
Stephan <stephan.mic@gmail.com>
2014-12-05 15:37:27 +0100

    update

--- bin/boot.sh
+++ bin/boot.sh
@@ -15,10 +15,6 @@
 export APP_ROOT=$HOME
 export LD_LIBRARY_PATH=$APP_ROOT/lua/lib:$APP_ROOT/nginx/lib:$LD_LIBRARY_PATH
 
-ls -l $APP_ROOT
-
-cp $APP_ROOT/lua/lib/* $APP_ROOT/nginx/lib
-
 conf_file=$APP_ROOT/nginx/conf/nginx.conf
 if [ -f $APP_ROOT/public/nginx.conf ]
 then
@@ -33,7 +29,7 @@ erb $APP_ROOT/nginx/conf/orig.conf > $APP_ROOT/nginx/conf/nginx.conf
 touch $APP_ROOT/nginx/logs/access.log
 touch $APP_ROOT/nginx/logs/error.log
 
-sed -i 's#80#'"${VCAP_APP_PORT}"'#' $APP_ROOT/nginx/conf/nginx.conf
+#sed -i 's#80#'"${VCAP_APP_PORT}"'#' $APP_ROOT/nginx/conf/nginx.conf
 
 (tail -f -n 0 $APP_ROOT/nginx/logs/*.log &)
 exec $APP_ROOT/nginx/sbin/nginx -p $APP_ROOT/nginx -c $APP_ROOT/nginx/conf/nginx.conf
